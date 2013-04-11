4286c16b4502cab5f1c5ae5973a55c9ffc5f0c37
Sven Nilsen <bvssvni@gmail.com>
2013-04-11 17:43:24 +0200

    Added CairoFillModule.

---
+++ Utils/Modules/CairoFillModule.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Utils
+{
+	public class CairoFillModule
+	{
+		public static void Fill(Cairo.Context context, Gtk.Widget widget, Cairo.Color color) {
+			context.NewPath();
+			context.Color = color;
+			context.Rectangle(0, 0, widget.Allocation.Width, widget.Allocation.Height);
+			context.Fill();
+		}
+		
+		public static void Fill(Cairo.Context context, int width, int height, Cairo.Color color) {
+			context.NewPath();
+			context.Color = color;
+			context.Rectangle(0, 0, width, height);
+			context.Fill();
+		}
+	}
+}
+
