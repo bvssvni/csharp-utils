a55d67a094bc4a0727b4c4ceb99d6f8446412da5
Sven Nilsen <bvssvni@gmail.com>
2013-07-25 17:30:13 +0200

    Updated 'Utils.Persistency'.

--- Utils/Packs/Utils.Persistency.cs
+++ Utils/Packs/Utils.Persistency.cs
@@ -4,9 +4,14 @@ Utils.Persistency - Classes that remember previous states.
 BSD license.
 by Sven Nilsen, 2013
 http://www.cutoutpro.com
-Version: 0.001 in angular degrees version notation
+Version: 0.002 in angular degrees version notation
 http://isprogrammingeasy.blogspot.no/2012/08/angular-degrees-versioning-notation.html
 
+
+
+0.002 - Added 'GetValue' and 'SetValue' to make subclassing of PersistentDictionary easier.
+		Added 'CopyMembers' to create new references of all members.
+		This is used in 'UndoRedo' to create future state from current.
 0.001 - Added 'UndoRedo'.
 
 Redistribution and use in source and binary forms, with or without
@@ -35,8 +40,29 @@ either expressed or implied, of the FreeBSD Project.
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// When inheriting from PersistentDictionary or PersistentList,
+/// remember to override the 'Copy' method.
+/// 
+/// public override PersistentDictionary<string, object> Copy(PersistentDictionary<string, object> to)
+/// {
+///		if (to == null)
+///		{
+///			to = new Frame ();
+///		}
+///			
+///		return base.Copy(to);
+///	}
+/// </summary>
 namespace Utils.Persistency
 {
+	public interface IStoreRestore
+	{
+		void Store();
+		void Restore();
+		void CopyMembers();
+	}
+	
 	/// <summary>
 	/// Persistent object.
 	/// 
@@ -45,12 +71,13 @@ namespace Utils.Persistency
 	/// 
 	/// It is important that 'Store' and 'Restore' is called equal number of times.
 	/// </summary>
-	public abstract class PersistentBase<T> where T : PersistentBase<T>
+	public abstract class PersistentBase<T> : IStoreRestore where T : PersistentBase<T>
 	{
 		private T m_previous;
 		private int m_noChange = 0;
 		
 		public abstract T Copy (T to = default (T));
+		public abstract void CopyMembers ();
 		public abstract bool HasChanged (T obj);
 		
 		public abstract void StoreMembers ();
@@ -88,7 +115,7 @@ namespace Utils.Persistency
 			RestoreMembers ();
 		}
 	}
-
+	
 	/// <summary>
 	/// Persistent value.
 	/// 
@@ -114,6 +141,15 @@ namespace Utils.Persistency
 			return to;
 		}
 		
+		public override void CopyMembers()
+		{
+			if (Value is IStoreRestore) {
+				var copyMethod = Value.GetType ().GetMethod("Copy");
+				Value = (T)copyMethod.Invoke (Value, new object [] {null});
+				((IStoreRestore)Value).CopyMembers ();
+			}
+		}
+		
 		public override bool HasChanged(PersistentValue<T> obj)
 		{
 			return obj == null || !this.Value.Equals (obj.Value);
@@ -121,15 +157,19 @@ namespace Utils.Persistency
 		
 		public override void StoreMembers()
 		{
-			
+			if (Value is IStoreRestore) {
+				((IStoreRestore)Value).Store();
+			}
 		}
 		
 		public override void RestoreMembers()
 		{
-			
+			if (Value is IStoreRestore) {
+				((IStoreRestore)Value).Restore();
+			}
 		}
 	}
-
+	
 	/// <summary>
 	/// List of persistent objects.
 	/// </summary>
@@ -146,6 +186,15 @@ namespace Utils.Persistency
 			to.m_list.Clear ();
 			to.m_list.AddRange (m_list);
 			return to;
+		}
+		
+		public override void CopyMembers()
+		{
+			int n = m_list.Count;
+			for (int i = 0; i < n; i++) {
+				m_list [i] = m_list [i].Copy ();
+				m_list [i].CopyMembers ();
+			}
 		}
 		
 		public override bool HasChanged (PersistentList<T> obj) {
@@ -240,7 +289,7 @@ namespace Utils.Persistency
 		{
 			int index = IndexOf (item);
 			if (index == -1) {return false;}
-
+			
 			m_list.RemoveAt (index);
 			return true;
 		}
@@ -262,7 +311,7 @@ namespace Utils.Persistency
 		}
 #endregion
 	}
-
+	
 	/// <summary>
 	/// Persistent dictionary.
 	/// </summary>
@@ -271,6 +320,36 @@ namespace Utils.Persistency
 		IDictionary<TKey, TValue>
 	{
 		private Dictionary<TKey, PersistentValue<TValue>> m_dict;
+		
+		protected T GetValue<T>(TKey key, T defaultValue) where T : TValue
+		{
+			if (ContainsKey(key))
+			{
+				return (T)this[key];
+			}
+			else
+			{
+				return defaultValue;
+			}
+		}
+		
+		protected void SetValue<T>(TKey key, T defaultValue, T val) where T : TValue
+		{
+			if (val == null || val.Equals(defaultValue))
+			{
+				Remove(key);
+				return;
+			}
+			
+			if (ContainsKey(key))
+			{
+				this[key] = val;
+			}
+			else 
+			{
+				Add(key, val);
+			}
+		}
 		
 		public override PersistentDictionary<TKey, TValue> Copy(PersistentDictionary<TKey, TValue> to = null)
 		{
@@ -285,23 +364,34 @@ namespace Utils.Persistency
 			
 			return to;
 		}
-
+		
+		public override void CopyMembers()
+		{
+			var keys = new TKey [m_dict.Count];
+			m_dict.Keys.CopyTo (keys, 0);
+			foreach (var key in keys) {
+				var copy = m_dict [key].Copy ();
+				m_dict [key] = copy;
+				copy.CopyMembers ();
+			}
+		}
+		
 		private static bool AreDifferentKeys(Dictionary<TKey, PersistentValue<TValue>> a,
-		                                 Dictionary<TKey, PersistentValue<TValue>> b) {
+		                                     Dictionary<TKey, PersistentValue<TValue>> b) {
 			foreach (var pair in a) {
 				if (b.ContainsKey (pair.Key)) {
 					return true;
 				}
 			}
-
+			
 			return false;
 		}
-
+		
 		public override bool HasChanged(PersistentDictionary<TKey, TValue> obj)
 		{
 			return obj == null 
 				|| m_dict.Count != obj.m_dict.Count 
-				|| AreDifferentKeys (this.m_dict, obj.m_dict);
+					|| AreDifferentKeys (this.m_dict, obj.m_dict);
 		}
 		
 		public override void RestoreMembers()
@@ -419,7 +509,7 @@ namespace Utils.Persistency
 		}		
 #endregion
 	}
-
+	
 	/// <summary>
 	/// Undo redo.
 	/// 
@@ -500,7 +590,8 @@ namespace Utils.Persistency
 				m_future.Store ();
 			}
 			
-			m_future = m_current.Copy (m_future);
+			m_future = (T)m_current.Copy ();
+			m_future.CopyMembers ();
 			m_future.Store ();
 			m_current.Restore ();
 		}
